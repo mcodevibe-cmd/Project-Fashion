161fddf0aa00b55129837c18d7fdf0f8929e3790
Muaj <m.codevibe@gmail.com>
2025-08-21 18:25:12 +0600

    1st Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let slideIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+document.querySelector('.next').addEventListener('click', () => {
+  slideIndex = (slideIndex + 1) % totalSlides;
+  updateSlide();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
+  updateSlide();
+});
+
+function updateSlide() {
+  slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
